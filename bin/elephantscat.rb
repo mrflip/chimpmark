aea13e378b63f5772db8264478ad6905c05c80c9
Philip (flip) Kromer <flip@infochimps.org>
2010-06-20 04:25:01 +0000

    Made elephant_scat archive files in a well-mannered tree

--- bin/elephantscat.rb
+++ bin/elephantscat.rb
@@ -2,7 +2,12 @@
 require 'rubygems'
 require 'wukong'
 require 'imw'
+require 'configliere'
+require 'configliere/commandline'
+require 'fileutils'
 
+Settings.define :archive_dir, :default => File.expand_path("~/timings"), :description => 'Where to store the archived job output'
+Settings.resolve!
 
 #
 # FIXME: add these fields: started at, num_tasks, reduce_input_groups
@@ -24,36 +29,30 @@ class JobDetails < TypedStruct.new(
     [:map_input_records,     Bignum],
     [:map_output_records,    Bignum],
     [:reduce_input_records,  Bignum],
-    [:reduce_output_records, Bignum]
+    [:reduce_output_records, Bignum],
+    [:job_name,              String]
     )
 end
 
 class JobDetailsParser
 
-  def initialize *args
-    
-  end
-  
-  def extract_run_time raw
+  def extract_header raw, intro
     return if raw.blank?
-    re = /^\<b\>Finished\Win\:\<\/b\>\W(.*)\</
-    run_time = (raw.match(re)).captures.first rescue nil
+    re = /^\<b\>#{intro}\:\<\/b\>\W*(.*)\</
+    (raw.match(re)).captures.first rescue nil
   end
 
   def extract_finished raw
     return if raw.blank?
     re = /^\<b\>Status\:\<\/b\>\W(.*)\</
     finished = (raw.match(re)).captures.first rescue nil
-    return 1 unless finished.downcase == 'failed'
-    0
+    (finished.to_s.downcase == 'failed') ? 0 : 1
   end
 
   def extract_raw_table imw_obj
-    ripd = imw_obj.parse ["table[2]/tr", {:row => ["td"]}]
+    ripd = imw_obj.parse ["table[2]//tr", {:row => ["td"]}]
     rawd = ripd.map do |ele|
-      if ele[:row].size >= 5
-        ele[:row] = ele[:row][1..-1] #cut off the crap at the beginning
-      end      
+      ele[:row][-4..-1] #cut off the crap at the beginning
     end
     rawd
   end
@@ -63,43 +62,88 @@ class JobDetailsParser
   #
   def raw_table_to_hash tbl
     fixd_hsh = tbl.reject{|arr| arr.blank?}.inject({}) do |h,arr|
-      title = arr.first.downcase.gsub(' ', '_').gsub('-', '_').to_sym
-      h[title] = arr.last.gsub(",","")
+      title = arr.first.downcase.gsub(/[-\s]/, '_').to_sym
+      val   = arr.last.gsub(",","")
+      h[title] = val
       h
     end
     fixd_hsh
   end
 
-  def parse jobdetails, &blk
-    data       = IMW.open(jobdetails)
-    raw_string = IMW.open(jobdetails).read
-    run_time   = extract_run_time raw_string
+  def parse jobdetails_file, &blk
+    data       = IMW.open(jobdetails_file)
+    raw_string = IMW.open(jobdetails_file).read
     finished   = extract_finished raw_string
     rawd       = extract_raw_table data
     fixd       = raw_table_to_hash rawd
-    fixd[:job_id]   = jobdetails.gsub('.html', '')
-    fixd[:run_time] = run_time
+    fixd[:job_id]   = File.basename(jobdetails_file).gsub('.html', '')
+    fixd[:run_time] = extract_header( raw_string, '(?:Finished|Killed) in')
+    fixd[:job_name] = extract_header( raw_string, 'Job Name')
     fixd[:finished] = finished
-    yield JobDetails.from_hash fixd
+    yield JobDetails.from_hash(fixd)
   end
-  
+
 end
 
-#
-# Use curl to fetch the jobdetails page
-#
-def get_job_pages job_id
-  details_url  = NAMENODE_URL + ":50030" + "/jobdetails.jsp?jobid=" + job_id
-  details_file = job_id + ".html"
-  jobconf_url  = NAMENODE_URL + ":50030" + "/jobconf.jsp?jobid=" + job_id
-  jobconf_file = job_id + ".xml"
-  %x{curl -s #{details_url} -o #{details_file}}
-  %x{curl -s #{jobconf_url} -o #{jobconf_file}}
+class ArchivableJob
+  attr_reader :job_id
+  def initialize job_id
+    @job_id = job_id
+    if job_id.blank? then warn "Please give a job_id: something like 'job_201006162228_0021'" ; exit(-2) ; end
+  end
+
+  # the jobtrackerid_jobcount part of the job_id:
+  # @example ArchivableJob.new('job_201006162228_0021').slug  # => '201006162228_0021'
+  def slug
+    job_id.gsub(/^job_/, '')
+  end
+
+  def archive_path
+    File.join(Settings.archive_dir, job_id.to_s.gsub(/_/, '/'))
+  end
+  # Where to put the parsed statistics.
+  def stats_filename
+    File.join(archive_path, "stats-#{slug}.tsv")
+  end
+  def jobdetails_filename
+    File.join(archive_path, "jobdetails-#{slug}.html")
+  end
+  def jobconf_filename
+    File.join(archive_path, "jobconf-#{slug}.xml")
+  end
+
+  # Where to find the jobdetails HTML page (the one that is parsed)
+  def jobdetails_url
+    NAMENODE_URL + ":50030" + "/jobdetails.jsp?jobid=" + job_id
+  end
+  # Where to find the jobconf xml
+  def jobconf_url
+    NAMENODE_URL + ":50030" + "/jobconf.jsp?jobid=" + job_id
+  end
+
+  #
+  # Use curl to fetch the jobdetails page
+  #
+  def archive!
+    FileUtils.mkdir_p(File.dirname(jobdetails_filename))
+    FileUtils.mkdir_p(File.dirname(jobconf_filename))
+    $stderr.print %x{curl -s #{jobdetails_url} -o #{jobdetails_filename}}
+    $stderr.print %x{curl -s #{jobconf_url}    -o #{jobconf_filename}}
+  end
+
+  def parse &block
+    parser = JobDetailsParser.new
+    File.open(stats_filename, 'w') do |stats_file|
+      parser.parse(jobdetails_filename)  do |details|
+        puts details.to_flat.join("\t")
+        stats_file << details.to_flat.join("\t")+"\n"
+        yield details if block
+      end
+    end
+  end
 end
 
-job_id = ARGV[0]
-get_job_pages job_id
-parser = JobDetailsParser.new
-parser.parse(job_id + ".html") do |details|
- puts details.to_flat.join("\t")
-end
+job_id = Settings.rest.first
+job = ArchivableJob.new(job_id)
+job.archive!
+job.parse
